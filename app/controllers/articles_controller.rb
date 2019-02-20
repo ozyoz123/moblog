f63362d1855cdab83f9f0e9bb3d0389babc5eab8
abdi <abdi@abdi.attlocal.net>
2019-02-19 21:43:45 -0500

    style updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
     @article.destroy
     redirect_to articles_path
-    flash[:notice] = "Article Was Successfuly deleted"
+    flash[:danger] = "Article Was Successfuly deleted"
   end
 
   private
