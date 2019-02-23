f22b69161de984eddeece49b2240704d1aadc43c
abdi <abdi@abdi.attlocal.net>
2019-02-23 01:16:18 -0500

    edit user records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] =  "Welcome To The Blog #{@user.username}"
+      flash[:success] =  "Welcome to the blog #{@user.username}"
       redirect_to articles_path
 
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account has been updated succuessfully"
+      redirect_to articles_path
+
+    else
+      render 'edit'
     end
   end
 
