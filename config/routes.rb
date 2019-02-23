5e3a31434f4c7596f05c02b3119bc5c9e08a1c93
abdi <abdi@abdi.attlocal.net>
2019-02-23 00:29:57 -0500

    signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+root 'pages#home'
+get 'about', to: 'pages#about'
 
   resources :articles
-  get 'about' => 'home#about'
-  get 'contact' => 'home#contact'
-  get 'team' => 'home#team'
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+
 end
