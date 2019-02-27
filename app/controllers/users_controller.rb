2a9d56a1b1a905f51f6d4c76c832e8e859148be2
abdi <abdi@abdi.attlocal.net>
2019-02-26 23:18:36 -0500

    Authentication Methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-
+before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page],per_page: 5)
   end
@@ -21,11 +21,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account has been updated succuessfully"
       redirect_to articles_path
@@ -36,7 +34,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -45,4 +42,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
 
 
 
